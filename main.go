29fabd6bda59c39f9e83ceed58d55457449cac9c
Капрелев Влад <v.kaprelev@MacBook-Pro-Administrator-2.local>
2025-01-03 12:46:13 +0700

    init http server

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"log"
 	"time"
 
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
-	"github.com/vladkaprelev/finance-go/db"
+	"github.com/vladkaprelev/finance-go/pool"
 )
 
 func main() {
@@ -18,11 +22,15 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	pool, err := db.NewPool(ctx)
-	if err != nil {
-		log.Fatalf("Ошибка при инициализации базы: %v", err)
-	}
-	defer pool.Close()
+	pool.InitPool(ctx)
 
-	log.Println("Подключение к базе PostgreSQL установлено!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World 5!"))
+	})
+	err = http.ListenAndServe(":3000", r)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
